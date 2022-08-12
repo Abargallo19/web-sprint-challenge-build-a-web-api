01ac9eca66531591a546686e4046730c72b1b61c
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-12 01:59:53 -0400

    GET project by id functional and DRY

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -6,24 +6,29 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
-    // proModel.get()
-    // .then(projects => {
-    //     res.status(200).json(projects);
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    //     res.status(500).json({message: 'Something went wrong on our end'})
-    // });
     try {
         const allProjects = await proModel.get()
-        if(allProjects) return res.status(200).json(allProjects)
+        if (allProjects) return res.status(200).json(allProjects)
     } catch (error) {
-        
+        res.status(500).json({ message: 'Something went wrong on our end' })
     }
 
-
-    
 });
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const projectSelect = await proModel.get(req.params.id)
+        if (!projectSelect) {
+            res.status(404).json({ message: 'Houston, we have a problem...that doesnt exist.' })
+        } else {
+            res.json(projectSelect)
+        }
+    } catch (error) {
+
+    }
+
+})
 
 
 
