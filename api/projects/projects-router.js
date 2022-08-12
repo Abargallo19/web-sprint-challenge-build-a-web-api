cc1a7a739df006f99bc8e7aa621bb27b0d0129d6
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-12 01:40:51 -0400

    refactored project GET to DRY

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,17 +5,36 @@ const proModel = require('./projects-model');
 const router = express.Router();
 
 
+router.get('/', async (req, res) => {
+    // proModel.get()
+    // .then(projects => {
+    //     res.status(200).json(projects);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    //     res.status(500).json({message: 'Something went wrong on our end'})
+    // });
+    try {
+        const allProjects = await proModel.get()
+        if(allProjects) return res.status(200).json(allProjects)
+    } catch (error) {
+        
+    }
 
 
-router.get('/', (req, res) => {
-    proModel.get()
-    .then(projects => {
-        res.status(200).json(projects);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({message: 'Something went wrong on our end'})
-    });
     
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
