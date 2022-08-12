a3c50fb6a949a324224c0d013592957a9e549459
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-12 16:44:08 -0400

    action POST functional but wont pass 400 test

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -21,19 +21,30 @@ router.get('/:id', async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong on our end' })
     }
-})
+});
 
 
+router.post('/', async (req, res) => {
+    try {
+        const { project_id, description, notes } = req.body;
+        const action = await actionMod.insert({ project_id, description, notes });
+        if (!project_id || !description || !notes) {
+            res.status(400).json({ message: 'missing reqs' })
+        } else {
+            res.status(201).json(action)
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: "uh oh" })
+    }
+});
 
+router.delete('/:d', (req, res) => {
 
+});
+router.put('/:id', (req, res) => {
 
-
-
-
-
-
-
-
+});
 
 
 module.exports = router;
