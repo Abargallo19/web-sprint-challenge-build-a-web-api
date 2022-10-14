c5136990692fa4170b96604729bda77e77612237
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-10-14 00:22:05 -0400

    uniform code for server build

--- api/server.js
+++ api/server.js
@@ -3,13 +3,15 @@ const server = express();
 
 server.use(express.json());
 
-const projectRouter = require('./projects/projects-router')
+const projectRouter = require('./projects/projects-router');
 server.use('/api/projects', projectRouter);
 
-const actionRouter = require('./actions/actions-router')
-server.use('/api/actions', actionRouter)
+const actionRouter = require('./actions/actions-router');
+server.use('/api/actions', actionRouter);
 
 module.exports = server;
+
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
